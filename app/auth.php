92df98c740101a2d54adbc696383fb9291dd38ec
IamCarron <dcarron32@gmail.com>
2024-03-09 20:15:37 +0100

    FixedLogin

--- app/auth.php
+++ app/auth.php
@@ -13,18 +13,24 @@ if (!isset($_POST['submit'])) {
 }
 
 $conn = mysqli_connect('mysql:3306', 'root', 'abc123.', 'login');
+if (!$conn) {
+    die("Error on the conection with the database: " . mysqli_connect_error());
+}
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 
 $result = mysqli_query($conn, $query);
+if (!$result) {
+    die("Error into the query: " . mysqli_error($conn));
+}
 $users_num = mysqli_num_rows($result);
 
 if ($users_num < 1) {
-  $_SESSION['login_failed'] = true;
-  header('location: ./index.php');
-  exit;
+    $_SESSION['login_failed'] = true;
+    header('location: ./index.php');
+    exit;
 }
 
 $user = mysqli_fetch_assoc($result);
